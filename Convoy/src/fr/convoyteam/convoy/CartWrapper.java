c63d522b7285365e3a11a964ac9b87a0cba2f831
pctotitou <pctotitou@pctotitou-PC>
2020-05-18 11:35:18 +0200

    cercle de capture autour du cart, j'abandonne l'idée du trait de capture

--- Convoy/src/fr/convoyteam/convoy/CartWrapper.java
+++ Convoy/src/fr/convoyteam/convoy/CartWrapper.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -14,6 +15,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
@@ -28,52 +30,63 @@ public class CartWrapper implements Listener {
 	private Location spawnLocation;
 	private Location endLocation;
 	private Main mainref;
-	private final byte SQUARED_MAX_PUSH_RANGE = 4;
+	private final byte MAX_PUSH_RANGE = 2;
 	private int TOTAL_RAIL;
 	private Block previousBlock;
 	private int railNumber;
-	private ArrayList<Material> railList = new ArrayList<Material>();
+	private final ArrayList<Vector> vectorForParticles = fillPushVectorList(20);
 	public CartWrapper(Main _mainref, Location _spawnLocation, Location _endLocation, Integer _TOTAL_RAIL) {
 		mainref=_mainref;
 		spawnLocation=_spawnLocation;
 		endLocation=_endLocation;
 		TOTAL_RAIL=_TOTAL_RAIL;
 		world=spawnLocation.getWorld();
-		fillRailList();
 		summonCart();
 	}
-	public void fillRailList() {
-		railList.add(Material.RAIL);
-		railList.add(Material.ACTIVATOR_RAIL);
-		railList.add(Material.DETECTOR_RAIL);
-		railList.add(Material.POWERED_RAIL);
+	/**
+	 * Destroy the cart
+	 */ 
+	public void destroyCart() {
+		cart.remove();
 	}
-	
+	/**
+	 * Unregister the cart
+	 */
+	public void unregister(){
+		  HandlerList.unregisterAll(this);
+	}
+	/**
+	 * To tick for particles, messages and cart end detection
+	 */
 	public void tick() {
+		for(Vector vector : vectorForParticles) {
+			world.spawnParticle(Particle.END_ROD, cart.getLocation().add(vector), 1);
+		}
 		if(getPushersInRange().size()>=1) {
 			if(cart.getMaxSpeed()==0) {
 				cart.setMaxSpeed(0.4);
 			}
 			cart.setVelocity(new Vector(0,0.05,0));	
+			showAdvancementForPushers("[Pushing]");
+			return;
 		}
-		showAdvancementForPushers();
+		showAdvancementForPushers("[Standby]");
 		
 	}
-	public void showAdvancementForPushers() {
+	private void showAdvancementForPushers(String cartStatus) {
 		for(Player pusher : mainref.getPushers()) {
-			pusher.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.DARK_GREEN+"Advancement: "+ChatColor.GOLD+Math.round((railNumber/TOTAL_RAIL)*100)+"%"));
+			pusher.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.DARK_GREEN+"cartStatus: "+ChatColor.GOLD+Math.round((railNumber/TOTAL_RAIL)*100)+"%"));
 		}
 	}
-	public ArrayList<Player> getPushersInRange(){
+	private ArrayList<Player> getPushersInRange(){
 		ArrayList<Player> pushersInRange = new ArrayList<Player>();
 		for(Player pusher : mainref.getPushers()) {
-			if(cart.getLocation().distanceSquared(pusher.getLocation())<=SQUARED_MAX_PUSH_RANGE) {
+			if(cart.getLocation().distanceSquared(pusher.getLocation())<=MAX_PUSH_RANGE*MAX_PUSH_RANGE) {
 				pushersInRange.add(pusher);
 			}
 		}
 		return pushersInRange;
 	}
-	
 	public void summonCart() {
 		Float yaw = getYawForLocation();
 		Location spawnLoc = spawnLocation;
@@ -81,8 +94,7 @@ public class CartWrapper implements Listener {
 		cart = (Minecart)world.spawnEntity(spawnLoc, EntityType.MINECART_TNT);
 		cart.setInvulnerable(true);
 	}
-	
-	public Float getYawForLocation() {
+	private Float getYawForLocation() {
 		BlockFace direction = getRailDirection();
 		switch(direction) {
 		case NORTH:
@@ -97,9 +109,13 @@ public class CartWrapper implements Listener {
 			return null;
 		}
 	}
-	
-	public BlockFace getRailDirection() {
+	private BlockFace getRailDirection() {
 		Shape railShape = ((Rail)spawnLocation.getBlock().getBlockData()).getShape();
+		ArrayList<Material> railList = new ArrayList<Material>();
+		railList.add(Material.RAIL);
+		railList.add(Material.ACTIVATOR_RAIL);
+		railList.add(Material.DETECTOR_RAIL);
+		railList.add(Material.POWERED_RAIL);
 		switch(railShape) {
 		case NORTH_SOUTH:
 			if(railList.contains(world.getBlockAt(spawnLocation.add(0, 0, -1)).getType())) {
@@ -115,11 +131,23 @@ public class CartWrapper implements Listener {
 			//LES RAILS NE SONT PAS BONS
 			mainref.stopGame();
 			return null;
+		}	
+	}
+	private ArrayList<Vector> fillPushVectorList(int nbPointsParRayon) {
+		ArrayList<Vector> vectorList = new ArrayList<Vector>();
+		float pas = MAX_PUSH_RANGE/nbPointsParRayon;
+		for (float x=0f; x<=MAX_PUSH_RANGE; x=x+pas) {
+			double alpha = Math.acos(x/2);
+			double y = Math.sin(alpha)*2;
+			vectorList.add(new Vector(x,0,y));
+			vectorList.add(new Vector(-x,0,y));
+			vectorList.add(new Vector(x,0,-y));
+			vectorList.add(new Vector(-x,0,-y));
 		}
-		
+		return vectorList;
 	}
 	@EventHandler
-	public void onCartMove(VehicleMoveEvent event) {
+	private void onCartMove(VehicleMoveEvent event) {
 		if(event.getVehicle()==cart) {
 			if(cart.getLocation()==endLocation){
 				mainref.stopGame();
@@ -135,9 +163,8 @@ public class CartWrapper implements Listener {
 			}
 		}
 	}
-	
 	@EventHandler
-	public void onMinecartCollision(VehicleEntityCollisionEvent event) {
+	private void onMinecartCollision(VehicleEntityCollisionEvent event) {
 		if(event.getVehicle()==cart) {
 			event.setCollisionCancelled(true);
 		}

208507f0522539332af8c639e4c72859fdff1f85
XxGoldenbluexX <tonyg222@outlook.fr>
2020-05-26 23:17:21 +0200

    butterfly

--- Convoy/src/fr/convoyteam/convoy/InterfaceManager.java
+++ Convoy/src/fr/convoyteam/convoy/InterfaceManager.java
@@ -1,7 +1,13 @@
 package fr.convoyteam.convoy;
 
+import java.util.WeakHashMap;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
 
 public class InterfaceManager implements Listener {
+	private final WeakHashMap<Player,Inventory> playersWeaponMenu = new WeakHashMap<Player,Inventory>();
 	
+
 }
--- Convoy/src/fr/convoyteam/convoy/weapons/ButterflyKnife.java
+++ Convoy/src/fr/convoyteam/convoy/weapons/ButterflyKnife.java
@@ -1,15 +1,28 @@
 package fr.convoyteam.convoy.weapons;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import fr.convoyteam.convoy.BaseWeapon;
+import fr.convoyteam.convoy.Main;
+import fr.convoyteam.convoy.enums.Team;
+import fr.convoyteam.convoy.enums.WeaponList;
 
 public class ButterflyKnife extends BaseWeapon {
 
-	@Override
-	public ItemStack getMenuItem() {
-		// TODO Auto-generated method stub
-		return null;
+
+	public ButterflyKnife(Main main, Player own, Team ownT, WeaponList list) {
+		super(main, own, ownT, list);
 	}
 
+	@Override
+	protected ItemStack makeTheGun() {
+		ItemStack itm = new ItemStack(Material.IRON_SWORD);
+		ItemMeta meta = itm.getItemMeta();
+		meta.setDisplayName(ChatColor.RED+getMMainRef().getText(getOwner(), "weapons.butterflyknife.name"));
+		return null;
+	}
 }
